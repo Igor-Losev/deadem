94fcf7b339c2e174c904674396ca827e6748ca53
Igor-Losev <igor.losev.it@gmail.com>
2024-11-08 13:02:32 +0100

    Added Class

---
+++ src/data/fields/Class.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('node:assert/strict');
+
+const Serializer = require('./Serializer');
+
+class Class {
+    constructor(id, name, serializer) {
+        assert(Number.isInteger(id));
+        assert(typeof name === 'string' && name.length > 0);
+        assert(serializer instanceof Serializer);
+
+        this._id = id;
+        this._name = name;
+        this._serializer = serializer;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get serializer() {
+        return this._serializer;
+    }
+}
+
+module.exports = Class;
