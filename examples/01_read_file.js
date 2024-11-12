17e36e6b98025ec380f56984642a3d07067a7ac4
Igor-Losev <igor.losev.it@gmail.com>
2024-11-12 16:52:48 +0100

    Added two example scripts

---
+++ examples/01_read_file.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('node:fs'),
+    path = require('node:path'),
+    Stream = require('node:stream');
+
+const Parser = require('./../src/Parser');
+
+(async () => {
+    const DEMO_PATH = path.resolve(__dirname, './../demos/21438112.dem');
+    const PARSER_THREADS = 1;
+
+    const buffer = fs.readFileSync(DEMO_PATH);
+
+    const parser = new Parser(PARSER_THREADS);
+
+    const reader = Stream.Readable.from(buffer);
+
+    parser.start(reader);
+})();
---
+++ examples/02_read_file_stream.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('node:fs'),
+    path = require('node:path');
+
+const Parser = require('./../src/Parser');
+
+(async () => {
+    const DEMO_PATH = path.resolve(__dirname, './../demos/21438112.dem');
+    const PARSER_THREADS = 1;
+
+    const parser = new Parser(PARSER_THREADS);
+
+    const reader = fs.createReadStream(DEMO_PATH, { highWaterMark: 65 * 1024 });
+
+    parser.start(reader);
+})();
