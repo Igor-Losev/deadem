352572ac4ccc2d828ecfb1af6f6a57ca500a37e9
Igor-Losev <igor.losev.it@gmail.com>
2024-11-12 15:01:52 +0100

    Added StreamPhase enumeration

---
+++ src/data/enums/StreamPhase.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const assert = require('node:assert/strict');
+
+const registry = {
+    byCode: new Map()
+};
+
+class StreamPhase {
+    constructor(code, order) {
+        assert(typeof code === 'string' && code.length > 0);
+        assert(Number.isInteger(order) && order >= 0);
+
+        this._code = code;
+        this._order = order;
+
+        registry.byCode.set(code, this);
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    get order() {
+        return this._order;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {StreamPhase}
+     */
+    static get ANALYZE() {
+        return analyze;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {StreamPhase}
+     */
+    static get BATCH() {
+        return batch;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {StreamPhase}
+     */
+    static get COORDINATE() {
+        return coordinate;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {StreamPhase}
+     */
+    static get EXTRACT() {
+        return extract;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {StreamPhase}
+     */
+    static get PARSE() {
+        return parse;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {StreamPhase}
+     */
+    static get READ() {
+        return read;
+    }
+}
+
+const analyze = new StreamPhase('ANALYZE', 5);
+const batch = new StreamPhase('BATCH', 2);
+const coordinate = new StreamPhase('COORDINATE', 4);
+const extract = new StreamPhase('EXTRACT', 1);
+const parse = new StreamPhase('PARSE', 3);
+const read = new StreamPhase('READ', 0);
+
+module.exports = StreamPhase;
