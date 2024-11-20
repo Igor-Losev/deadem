ba0bb2c5c74b2f988383ce358f68fb95583e6b0d
Igor-Losev <igor.losev.it@gmail.com>
2024-11-20 17:33:44 +0100

    Added FieldPathExtractor to the log4js.config

--- log4js.config.js
+++ log4js.config.js
@@ -5,6 +5,7 @@ module.exports = {
     categories: {
         default: { appenders: [ 'console' ], level: 'info' },
 
+        FieldPathExtractor: { appenders: [ 'console' ], level: 'info' },
         Parser: { appenders: [ 'console' ], level: 'info' },
         PerformanceTracker: { appenders: [ 'console' ], level: 'info' },
         StringTableContainer: { appenders: [ 'console' ], level: 'info' },
