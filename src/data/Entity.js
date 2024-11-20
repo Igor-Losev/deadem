7295dadeef3eeb6616df4b9f4397978e631a814f
Igor-Losev <igor.losev.it@gmail.com>
2024-11-20 17:38:24 +0100

    Added initial version of Entity

---
+++ src/data/Entity.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('node:assert/strict');
+
+const Class = require('./fields/Class');
+
+class Entity {
+    constructor(index, serial, clazz) {
+        assert(Number.isInteger(index));
+        assert(Number.isInteger(serial));
+        assert(clazz instanceof Class);
+
+        this._index = index;
+        this._serial = serial;
+        this._class = clazz;
+    }
+
+    get index() {
+        return this._index;
+    }
+
+    get serial() {
+        return this._serial;
+    }
+
+    get class() {
+        return this._class;
+    }
+}
+
+module.exports = Entity;
