cb0bd4e74fe2f0bf618f24c640653397a02498e1
Igor-Losev <igor.losev.it@gmail.com>
2024-11-29 18:15:12 +0100

    Replaced FieldType with FieldDefinition; added tests

---
+++ src/data/fields/FieldDefinition.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const assert = require('node:assert/strict');
+
+const REGEX = /(?<base>[^*< >\n\[\]]+)(< (?<generic>.*) >)?(\[(?<count>\d+)?\])?(?<pointer>\*)?/;
+
+class FieldDefinition {
+    /**
+     * @public
+     * @constructor
+     * @param {String} baseType
+     * @param {FieldDefinition|null} generic
+     * @param {Number|null} count
+     * @param {boolean} pointer
+     */
+    constructor(baseType, generic, count, pointer) {
+        assert(typeof baseType === 'string' && baseType.length > 0);
+        assert(generic === null || generic instanceof FieldDefinition);
+        assert(count === null || Number.isInteger(count));
+        assert(typeof pointer === 'boolean');
+
+        this._baseType = baseType;
+        this._generic = generic;
+        this._count = count;
+        this._pointer = pointer;
+    }
+
+    get baseType() {
+        return this._baseType;
+    }
+
+    get generic() {
+        return this._generic;
+    }
+
+    get count() {
+        return this._count;
+    }
+
+    get pointer() {
+        return this._pointer;
+    }
+
+    /**
+     * @public
+     * @static
+     * @param {String} varType
+     * @returns {FieldDefinition}
+     */
+    static parse(varType) {
+        assert(typeof varType === 'string');
+
+        const groups = REGEX.exec(varType).groups;
+
+        const base = groups.base;
+        const generic = groups.generic ? FieldDefinition.parse(groups.generic) : null;
+        const count = groups.count ? parseInt(groups.count) : null;
+        const pointer = groups.pointer === '*';
+
+        return new FieldDefinition(base, generic, count, pointer);
+    }
+}
+
+module.exports = FieldDefinition;
---
+++ src/data/fields/test/FieldDefinition.test.js
@@ -0,0 +1,47 @@
+const FieldDefinition = require('./../FieldDefinition');
+
+describe('FieldDefinition.parse()', () => {
+    describe('When parsing "CNetworkUtlVectorBase< CHandle< CBaseEntity > >"', () => {
+        const definition = FieldDefinition.parse('CNetworkUtlVectorBase< CHandle< CBaseEntity > >');
+
+        test('It should have "CNetworkUtlVectorBase" as base type', () => {
+            expect(definition.baseType).toBe('CNetworkUtlVectorBase');
+        });
+
+        test('It should have generic - "CHandle< CBaseEntity >"', () => {
+            expect(definition.generic).not.toBeNull();
+            expect(definition.generic.baseType).toBe('CHandle');
+            expect(definition.generic.generic).not.toBeNull();
+            expect(definition.generic.generic.baseType).toBe('CBaseEntity');
+        });
+    });
+
+    describe('When parsing "CEntityIdentity*"', () => {
+        const definition = FieldDefinition.parse('CEntityIdentity*');
+
+        test('It should have "CEntityIdentity" as base type', () => {
+            expect(definition.baseType).toBe('CEntityIdentity');
+        });
+
+        test('It should have pointer set to true', () => {
+            expect(definition.pointer).toBe(true);
+        });
+    });
+
+    describe('When parsing "CHandle< CBaseEntity >[64]"', () => {
+        const definition = FieldDefinition.parse('CHandle< CBaseEntity >[64]');
+
+        test('It should have "CHandle" as base type', () => {
+            expect(definition.baseType).toBe('CHandle');
+        });
+
+        test('It should have "CBaseEntity" as generic base type', () => {
+            expect(definition.generic).not.toBeNull();
+            expect(definition.generic.baseType).toBe('CBaseEntity');
+        });
+
+        test('It should have a count of "64"', () => {
+            expect(definition.count).toBe(64);
+        });
+    });
+});
