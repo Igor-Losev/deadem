800968b6a2021ffc9851fb335b7df88fbbfa48f5
Igor-Losev <igor.losev.it@gmail.com>
2025-05-12 17:22:53 +0200

    Serializer.getDecoderForFieldPath()

--- src/data/fields/Serializer.js
+++ src/data/fields/Serializer.js
@@ -24,10 +24,16 @@ class Serializer {
         this._fields = fields;
     }
 
+    /**
+     * @returns {SerializerKey}
+     */
     get key() {
         return this._key;
     }
 
+    /**
+     * @returns {Array<Field>}
+     */
     get fields() {
         return this._fields;
     }
@@ -37,19 +43,29 @@ class Serializer {
      * @param {FieldPath} fieldPath
      * @param {Number=} fieldPathIndex
      */
-    getDecoder(fieldPath, fieldPathIndex = 0) {
+    getDecoderForFieldPath(fieldPath, fieldPathIndex = 0) {
         assert(fieldPath instanceof FieldPath);
         assert(Number.isInteger(fieldPathIndex));
 
         const fieldIndex = fieldPath.get(fieldPathIndex);
 
         if (!Number.isInteger(fieldIndex) || fieldIndex >= this._fields.length) {
-            throw new Error(`Unable to get decoder - field index [ ${fieldIndex} ] is out of bounds`);
+            throw new Error(`Unable to get decoder: field index [ ${fieldIndex} ] is out of bounds(${this._fields.length})`);
         }
 
         const field = this._fields[fieldIndex];
 
-        return field.getDecoder(fieldPath, fieldPathIndex + 1);
+        return field.getDecoderForFieldPath(fieldPath, fieldPathIndex + 1);
+    }
+
+    /**
+     * Pushes a {@link Field}.
+     *
+     * @public
+     * @param {Field} field
+     */
+    push(field) {
+        this._fields.push(field);
     }
 }
 
