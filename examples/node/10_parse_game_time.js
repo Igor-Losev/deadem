669046260d7255297d70282baba3d397f39af9af
Igor-Losev <igor.losev.it@gmail.com>
2025-05-29 15:16:32 +0200

    Added two more examples - 10 and 11

---
+++ examples/node/10_parse_game_time.js
@@ -0,0 +1,35 @@
+import { InterceptorStage, MessagePacketType, Parser, Printer } from '#root/index.js';
+
+import DemoFile from './helpers/DemoFile.js';
+import DemoProvider from './helpers/DemoProvider.js';
+
+(async () => {
+    const reader = await DemoProvider.read(DemoFile.MATCH_35244871);
+
+    const parser = new Parser();
+    const printer = new Printer(parser);
+
+    let gameTime = null;
+
+    parser.registerPostInterceptor(InterceptorStage.MESSAGE_PACKET, async (demoPacket, messagePacket) => {
+        if (messagePacket.type === MessagePacketType.NET_TICK) {
+            const demo = parser.getDemo();
+
+            if (demo.server === null) {
+                return;
+            }
+
+            const tick = messagePacket.data.tick;
+
+            const tickRate = demo.server.tickRate;
+
+            gameTime = tick / tickRate;
+        }
+    });
+
+    await parser.parse(reader);
+
+    printer.printStats();
+
+    console.log(`Game finished in [ ${gameTime} ] seconds`);
+})();
---
+++ examples/node/11_parse_top_damage_dealer.js
@@ -0,0 +1,37 @@
+import { InterceptorStage, MessagePacketType, Parser, Printer } from '#root/index.js';
+
+import DemoFile from './helpers/DemoFile.js';
+import DemoProvider from './helpers/DemoProvider.js';
+
+(async () => {
+    const reader = await DemoProvider.read(DemoFile.MATCH_35244871);
+
+    const parser = new Parser();
+    const printer = new Printer(parser);
+
+    const topDamageDealer = {
+        player: null,
+        damage: 0
+    };
+
+    parser.registerPostInterceptor(InterceptorStage.ENTITY_PACKET, async (demoPacket, messagePacket, events) => {
+        events.forEach((event) => {
+            const entity = event.entity;
+
+            if (entity.class.name === 'CCitadelPlayerController') {
+                const data = entity.unpackFlattened();
+
+                if (data.m_iHeroDamage > topDamageDealer.damage) {
+                    topDamageDealer.player = data.m_iszPlayerName;
+                    topDamageDealer.damage = data.m_iHeroDamage;
+                }
+            }
+        });
+    });
+
+    await parser.parse(reader);
+
+    printer.printStats();
+
+    console.log(`Top damage dealer is [ ${topDamageDealer.player} ] with [ ${topDamageDealer.damage} ] damage`);
+})();
