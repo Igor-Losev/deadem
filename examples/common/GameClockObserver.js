4f3b7a71e8a437ae9d8c4b38ec711c4bba4d3699
Igor-Losev <igor.losev.it@gmail.com>
2025-07-06 14:27:53 +0200

    Added GameClockObserver and GameState

---
+++ examples/common/GameClockObserver.js
@@ -0,0 +1,131 @@
+import { InterceptorStage, MessagePacketType } from '#root/index.js';
+
+const GAME_RULES_CLASS_NAME = 'CCitadelGameRulesProxy';
+const SECONDS_IN_MINUTE = 60;
+
+class GameClockObserver {
+    /**
+     * @constructor
+     * @param {Parser} parser
+     */
+    constructor(parser) {
+        this._parser = parser;
+
+        this._clock = 0;
+        this._pausesDuration = 0;
+
+        this._gameRulesReference = null;
+
+        this._initialize();
+    }
+
+    /**
+     * @public
+     * @returns {number}
+     */
+    get clock() {
+        return this._clock;
+    }
+
+    /**
+     * @public
+     * @returns {number}
+     */
+    get pausesDuration() {
+        return this._pausesDuration;
+    }
+
+    /**
+     * @public
+     * @returns {string}
+     */
+    getClockFormatted() {
+        return formatClock(this._clock);
+    }
+
+    /**
+     * @public
+     * @returns {string}
+     */
+    getPausesDurationFormatted() {
+        return formatClock(this._pausesDuration);
+    }
+
+    /**
+     * @protected
+     */
+    _initialize() {
+        const unregisterInterceptorForInitialization = () => this._parser.unregisterPostInterceptor(InterceptorStage.DEMO_PACKET, interceptorForInitialization);
+
+        const interceptorForInitialization = (demoPacket) => {
+            if (demoPacket.getIsInitial()) {
+                return;
+            }
+
+            const demo = this._parser.getDemo();
+
+            const entities = demo.getEntitiesByClassName(GAME_RULES_CLASS_NAME);
+
+            if (entities.length !== 1) {
+                throw new Error(`Unexpected entities.length [ ${entities.length} ]. Expected a single entity of type [ ${GAME_RULES_CLASS_NAME} ]`);
+            }
+
+            this._gameRulesReference = entities[0];
+
+            unregisterInterceptorForInitialization();
+        };
+
+        const interceptorForGameClock = (demoPacket, messagePacket) => {
+            if (this._gameRulesReference === null) {
+                return;
+            }
+
+            const demo = this._parser.getDemo();
+
+            if (demo.server === null) {
+                return;
+            }
+
+            if (messagePacket.type !== MessagePacketType.NET_TICK) {
+                return;
+            }
+
+            const gameRulesData = this._gameRulesReference.unpackFlattened();
+
+            const currentTick = messagePacket.data.tick;
+
+            const clockLastUpdatedAt = gameRulesData['m_pGameRules.m_flMatchClockAtLastUpdate'];
+            const clockLastUpdatedTick = gameRulesData['m_pGameRules.m_nMatchClockUpdateTick'];
+
+            const elapsed = (currentTick - clockLastUpdatedTick) * demo.server.tickInterval;
+
+            this._clock = clockLastUpdatedAt + elapsed;
+
+            const totalPausedTicks = gameRulesData['m_pGameRules.m_nTotalPausedTicks'];
+
+            this._pausesDuration = totalPausedTicks * demo.server.tickInterval;
+        };
+
+        this._parser.registerPostInterceptor(InterceptorStage.DEMO_PACKET, interceptorForInitialization);
+        this._parser.registerPostInterceptor(InterceptorStage.MESSAGE_PACKET, interceptorForGameClock);
+    }
+}
+
+/**
+ * @param {number} clock
+ * @returns {string}
+ */
+function formatClock(clock) {
+    const minutes = clock / SECONDS_IN_MINUTE;
+    const fullMinutes = Math.floor(minutes);
+
+    const seconds = minutes - fullMinutes;
+    const fullSeconds = Math.floor(SECONDS_IN_MINUTE * seconds);
+
+    const fullMinutesFormatted = fullMinutes.toString().padStart(2, '0');
+    const fullSecondsFormatted = fullSeconds.toString().padStart(2, '0');
+
+    return `${fullMinutesFormatted}:${fullSecondsFormatted}`;
+}
+
+export default GameClockObserver;
---
+++ examples/common/GameState.js
@@ -0,0 +1,117 @@
+const registry = new Map();
+
+/**
+ * Represents a specific game state during the match lifecycle.
+ *
+ * Sourced from: `CCitadelGameRulesProxy.m_pGameRules.m_eGameState`
+ */
+class GameState {
+    /**
+     * @constructor
+     * @param {number} code
+     * @param {string} name
+     */
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+
+        registry.set(code, this);
+    }
+
+    /**
+     * @public
+     * @returns {number}
+     */
+    get code() {
+        return this._code;
+    }
+
+    /**
+     * @public
+     * @returns {string}
+     */
+    get name() {
+        return this._name;
+    }
+
+    /**
+     * @public
+     * @returns {Array<GameState>}
+     */
+    static getAll() {
+        return Array.from(registry.values());
+    }
+
+    /**
+     * @public
+     * @static
+     * @param {number} code
+     * @returns {GameState|null}
+     */
+    static parse(code) {
+        return registry.get(code) || null;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {GameState}
+     */
+    static get INIT() {
+        return stateInit;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {GameState}
+     */
+    static get PLAYERS_LOADING() {
+        return statePlayersLoading;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {GameState}
+     */
+    static get UNKNOWN_3() {
+        return stateUnknown3;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {GameState}
+     */
+    static get PRE_GAME() {
+        return statePreGame;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {GameState}
+     */
+    static get GAME_IN_PROGRESS() {
+        return stateGameInProgress;
+    }
+
+    /**
+     * @public
+     * @static
+     * @returns {GameState}
+     */
+    static get POST_GAME() {
+        return statePostGame;
+    }
+}
+
+const stateInit = new GameState(1, 'STATE_INIT');
+const statePlayersLoading = new GameState(2, 'STATE_PLAYERS_LOADING');
+const stateUnknown3 = new GameState(3, 'STATE_UNKNOWN_3');
+const statePreGame = new GameState(4, 'STATE_PRE_GAME');
+const stateGameInProgress = new GameState(5, 'STATE_IN_PROGRESS');
+const statePostGame = new GameState(6, 'STATE_POST_GAME');
+
+export default GameState;
