cf283c4b3c40133d90e292697d3d99cb19182e51
Igor-Losev <igor.losev.it@gmail.com>
2025-07-22 10:49:12 +0300

    Added Queue

---
+++ src/core/Queue.js
@@ -0,0 +1,82 @@
+class Queue {
+    /**
+     * @constructor
+     */
+    constructor() {
+        this._head = null;
+        this._tail = null;
+
+        this._size = 0;
+    }
+
+    /**
+     * @public
+     * @returns {number}
+     */
+    get size() {
+        return this._size;
+    }
+
+    /**
+     * @public
+     * @param {*} data
+     */
+    enqueue(data) {
+        const queueItem = new QueueItem(data);
+
+        if (this._size === 0) {
+            this._head = queueItem;
+            this._tail = queueItem;
+        } else {
+            this._tail.next = queueItem;
+
+            queueItem.previous = this._tail;
+
+            this._tail = queueItem;
+        }
+
+        this._size++;
+    }
+
+    /**
+     * @public
+     * @returns {*}
+     */
+    dequeue() {
+        if (this._size === 0) {
+            throw new Error(`Unable to dequeue - queue size is [ ${this._size} ]`);
+        }
+
+        const queueItem = this._head;
+
+        if (this._size === 1) {
+            this._head = null;
+            this._tail = null;
+        } else {
+            const next = queueItem.next;
+
+            next.previous = null;
+
+            this._head = next;
+        }
+
+        this._size--;
+
+        return queueItem.data;
+    }
+}
+
+class QueueItem {
+    /**
+     * @constructor
+     * @param {*} data
+     */
+    constructor(data) {
+        this.data = data;
+
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+export default Queue;
---
+++ tests/Queue.test.js
@@ -0,0 +1,40 @@
+import { describe, expect, test } from 'vitest';
+
+import Queue from '#core/Queue.js';
+
+describe('Queue', () => {
+    const queue = new Queue();
+
+    let index = 0;
+
+    const factory = () => ({
+        index: index++,
+        data: Math.random()
+    });
+
+    const items = [ factory(), factory(), factory(), factory() ];
+
+    test('It should enqueue', () => {
+        items.forEach((item) => {
+            queue.enqueue(item);
+        });
+
+        expect(queue._head.data).toBe(items[0]);
+        expect(queue._tail.data).toBe(items[items.length - 1]);
+
+        expect(queue.size).toBe(items.length);
+    });
+
+    test('It should dequeue', () => {
+        items.forEach((item) => {
+            const dequeued = queue.dequeue();
+
+            expect(dequeued).toBe(item);
+        });
+
+        expect(queue._head).toBe(null);
+        expect(queue._tail).toBe(null);
+
+        expect(queue.size).toBe(0);
+    });
+});
