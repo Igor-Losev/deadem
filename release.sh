29f26f969c5d4184ecfa88fbf14900a0ed6943e5
Igor-Losev <igor.losev.it@gmail.com>
2025-08-21 13:27:33 +0200

    Tweak release script

--- release.sh
+++ release.sh
@@ -5,14 +5,16 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-VERSION=$1
+VERSION_OR_TYPE=$1
 
-npm version $VERSION --workspaces
-npm version $VERSION --no-git-tag-version
+npm version $VERSION_OR_TYPE --workspaces
+npm version $VERSION_OR_TYPE --no-git-tag-version
+
+VERSION=$(node -p "require('./package.json').version")
 
 git add ./package.json ./package-lock.json ./packages/**/package.json
 
 git commit -m "$VERSION"
 
-git tag v$VERSION -s
+git tag v$VERSION -s -m "$VERSION"
 
