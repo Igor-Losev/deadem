522fe7fc1665e097d4488c00b339c8fd4aae94ff
Igor-Losev <igor.losev.it@gmail.com>
2025-08-31 13:47:52 +0200

    Exported DeferredPromise from the library

--- packages/lib/index.js
+++ packages/lib/index.js
@@ -1,5 +1,6 @@
 export { default as BroadcastAgent } from './src/broadcast/BroadcastAgent.js';
 export { default as BroadcastGateway } from './src/broadcast/BroadcastGateway.js';
+export { default as DeferredPromise } from './src/data/DeferredPromise.js';
 export { default as DemoPacketType } from './src/data/enums/DemoPacketType.js';
 export { default as DemoSource } from './src/data/enums/DemoSource.js';
 export { default as EntityOperation } from './src/data/enums/EntityOperation.js';
@@ -11,3 +12,4 @@ export { default as ParserConfiguration } from './src/ParserConfiguration.js';
 export { default as Printer } from './src/Printer.js';
 export { default as Protocol } from './src/data/enums/Protocol.js';
 export { default as StringTableType } from './src/data/enums/StringTableType.js';
+
