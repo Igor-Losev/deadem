886233fc281b5321c53f32f4db3874c2c98a1925
Igor-Losev <igor.losev.it@gmail.com>
2026-02-03 12:12:30 +0100

    UI: improved Library style

--- packages/examples-ui/src/components/Library/Library.jsx
+++ packages/examples-ui/src/components/Library/Library.jsx
@@ -1,17 +1,21 @@
-import { Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Download as DownloadIcon } from '@mui/icons-material';
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 import DemoFile from 'deadem-examples-common/data/DemoFile.js';
 
 const FILES = DemoFile.getAll().reverse();
 
-const StyledTableRow = styled(TableRow)(() => ({
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: '#f1f1f1',
+    backgroundColor: theme.palette.action.hover,
   },
   '&:last-child td, &:last-child th': {
     border: 0,
   },
+  '&:hover': {
+    backgroundColor: theme.palette.action.selected,
+  }
 }));
 
 const S3_BUCKET_URL = 'https://deadem.s3.us-east-1.amazonaws.com/deadlock/demos';
@@ -26,35 +30,41 @@ function formatSize(bytes) {
 
 export default function Library() {
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label='files-table' stickyHeader size='small'>
+    <TableContainer component={Paper} elevation={0} sx={{ border: '1px solid', borderColor: 'divider' }}>
+      <Table aria-label='Demo files library' stickyHeader size='small'>
         <TableHead>
           <TableRow>
-            <TableCell sx={{ fontWeight: 'bold' }}>#</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Match</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Game Build</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Source</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Date</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Mode</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Size</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Link</TableCell>
+            <TableCell sx={{ fontWeight: 600, width: 40 }}>#</TableCell>
+            <TableCell sx={{ fontWeight: 600 }}>Match</TableCell>
+            <TableCell sx={{ fontWeight: 600 }}>Game Build</TableCell>
+            <TableCell sx={{ fontWeight: 600 }}>Source</TableCell>
+            <TableCell sx={{ fontWeight: 600 }}>Date</TableCell>
+            <TableCell sx={{ fontWeight: 600 }}>Mode</TableCell>
+            <TableCell sx={{ fontWeight: 600 }}>Size</TableCell>
+            <TableCell sx={{ fontWeight: 600, width: 60 }} align='center'>Link</TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
           {FILES.map((file, index) => (
             <StyledTableRow key={`${file.id}-${file.source.code}`}>
-              <TableCell>{index + 1}</TableCell>
+              <TableCell sx={{ color: 'text.secondary' }}>{index + 1}</TableCell>
               <TableCell>{file.id}</TableCell>
               <TableCell>{file.gameBuild ?? '-'}</TableCell>
               <TableCell>{file.source.code}</TableCell>
               <TableCell>{file.meta?.date ?? '-'}</TableCell>
               <TableCell>{file.meta?.mode ?? 'Normal'}</TableCell>
               <TableCell>{formatSize(file.meta?.size)}</TableCell>
-              <TableCell>
-                <Link href={`${S3_BUCKET_URL}/${file.getFileName()}`} target='_blank'>
-                  Download
-                </Link>
+              <TableCell align='center'>
+                <Tooltip title='Download .dem file' arrow>
+                  <IconButton
+                    href={`${S3_BUCKET_URL}/${file.getFileName()}`}
+                    size='small'
+                    target='_blank'
+                  >
+                    <DownloadIcon fontSize='small' />
+                  </IconButton>
+                </Tooltip>
               </TableCell>
             </StyledTableRow>
           ))}
