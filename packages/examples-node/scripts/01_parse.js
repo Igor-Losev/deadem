6d94dbfd8a50106c87a16e2c64d7fa95ea9d7773
Igor-Losev <igor.losev.it@gmail.com>
2026-02-14 18:40:38 +0100

    Examples: updated parse scripts; added play script

--- packages/examples-node/scripts/01_parse.js
+++ packages/examples-node/scripts/01_parse.js
@@ -5,12 +5,14 @@ import DemoFile from 'deadem-examples-common/data/DemoFile.js';
 import DemoProvider from '#root/providers/DemoProvider.js';
 
 (async () => {
-    const reader = await DemoProvider.read(DemoFile.REPLAY_38969017);
+    const reader = await DemoProvider.read(DemoFile.REPLAY_51541751);
 
-    const parser = new Parser(new ParserConfiguration({ parserThreads: 3 }));
+    const parser = new Parser(new ParserConfiguration({ parserThreads: 0 }));
     const printer = new Printer(parser);
 
     await parser.parse(reader);
+
+    await parser.dispose();
 
     printer.printStats();
 })();
