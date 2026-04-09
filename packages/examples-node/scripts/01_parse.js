127fa1b3fb1bd92e1a21914929dd44ff1d9f288f
Igor-Losev <igor.losev.it@gmail.com>
2026-04-09 14:24:00 +0200

    Added memory leak test script; modified 01_parse and 06_play

--- packages/examples-node/scripts/01_parse.js
+++ packages/examples-node/scripts/01_parse.js
@@ -5,15 +5,16 @@ import DemoFile from 'deadem-examples-common/data/DemoFile.js';
 import DemoProvider from '#root/providers/DemoProvider.js';
 
 (async () => {
-    const reader = await DemoProvider.read(DemoFile.REPLAY_51541751);
+    const reader = await DemoProvider.read(DemoFile.REPLAY_51541762);
 
     const parser = new Parser(new ParserConfiguration({ parserThreads: 0 }));
-    const printer = new Printer(parser);
 
     await parser.parse(reader);
-
     await parser.dispose();
 
-    printer.printStats();
-})();
+    const printer = new Printer(parser);
 
+    printer.printMemoryStats();
+    printer.printPerformanceStats();
+    printer.printPacketStats();
+})();
--- packages/examples-node/scripts/06_play.js
+++ packages/examples-node/scripts/06_play.js
@@ -1,4 +1,4 @@
-import { ParserConfiguration, Player } from 'deadem';
+import { ParserConfiguration, Player, Printer } from 'deadem';
 
 import DemoFile from 'deadem-examples-common/data/DemoFile.js';
 
@@ -7,29 +7,31 @@ import DemoProvider from '#root/providers/DemoProvider.js';
 (async () => {
     const reader = await DemoProvider.read(DemoFile.REPLAY_51541762);
 
-    const player = new Player(new ParserConfiguration({ parserThreads: 3 }));
-
-    const print = () => {
-        console.log(`Tick: [ ${player.ticks.current} ] out of [ ${player.ticks.last} ]`);
-    };
+    const player = new Player(new ParserConfiguration({ parserThreads: 0 }));
 
     await player.load(reader);
 
-    print();
-
-    await player.seekToTick(40234);
-
-    print();
+    console.log(`Loaded. Ticks: ${player.getFirstTick()} — ${player.getLastTick()}`);
+    console.log(`Current tick: ${player.getCurrentTick()}`);
 
     await player.nextTick();
+    console.log(`Next tick: ${player.getCurrentTick()}`);
 
-    player.abo
-
-    print();
+    await player.nextTick();
+    console.log(`Next tick: ${player.getCurrentTick()}`);
 
     await player.prevTick();
+    console.log(`Prev tick: ${player.getCurrentTick()}`);
 
-    print();
+    await player.seekToTick(20000);
+    console.log(`Seek to 20000: ${player.getCurrentTick()}`);
+
+    await player.seekToTick(player.getLastTick());
+    console.log(`Seek to end: ${player.getCurrentTick()}`);
 
     await player.dispose();
+
+    const printer = new Printer(player);
+
+    printer.printMemoryStats();
 })();
