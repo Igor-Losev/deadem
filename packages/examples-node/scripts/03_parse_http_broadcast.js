3d66d6053f1048b591ae150d16e9623a1ec4c8f9
Igor-Losev <igor.losev.it@gmail.com>
2026-04-14 13:44:11 +0200

    Corrected broadcast logic after removing axios; Made match_id -> string|number

--- packages/examples-node/scripts/03_parse_http_broadcast.js
+++ packages/examples-node/scripts/03_parse_http_broadcast.js
@@ -1,23 +1,27 @@
-import { BroadcastAgent, BroadcastGateway, DemoSource, Logger, Parser, ParserConfiguration, Printer } from 'deadem';
+import { BroadcastAgent, BroadcastGateway, DemoSource, InterceptorStage, Logger, Parser, ParserConfiguration, Printer } from 'deadem';
 
 (async () => {
     /**
      * Setup Instructions Before Running:
      *
-     * 1. Visit: https://deadlocktracker.gg/live
-     * 2. Find an ongoing match that has at least one spectator.
-     * 3. Verify that the following endpoint returns valid JSON data:
-     *    https://dist1-ord1.steamcontent.com/tv/{MATCH_ID}/sync
-     * 4. Replace the MATCH_ID variable in your script with the ID of the selected match.
+     * 1. Find a valid match identifier (connection target).
+     *    One of the ways to obtain it is via the Deadlock developer console
+     *    while connected to a server.
+     *
+     * 2. Verify that the following endpoint returns valid data:
+     *    https://dist1-ord1.steamcontent.com/tv/{MATCH_IDENTIFIER}/sync
+     *
+     * 3. Replace MATCH_ID in your script with the obtained identifier.
      */
     const FROM_BEGINNING = false;
-    const MATCH_ID = 39056521;
+    const MATCH_ID = '75637129_201673668';
 
     const broadcastGateway = new BroadcastGateway('dist1-ord1.steamcontent.com/tv');
     const broadcastAgent = new BroadcastAgent(broadcastGateway, MATCH_ID, Logger.CONSOLE_DEBUG);
 
     const parserConfiguration = new ParserConfiguration({ parserThreads: 3 });
     const parser = new Parser(parserConfiguration);
+
     const printer = new Printer(parser);
 
     await parser.parse(broadcastAgent.stream(FROM_BEGINNING), DemoSource.HTTP_BROADCAST);
