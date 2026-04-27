aec7e720a1e23165d7344e9fd15cac4e4d5b8dcc
Igor-Losev <igor.losev.it@gmail.com>
2026-04-27 20:41:36 +0200

    EntityMutationEvent: added getChanges

--- packages/engine/src/data/entity/EntityMutationEvent.js
+++ packages/engine/src/data/entity/EntityMutationEvent.js
@@ -9,6 +9,8 @@ class EntityMutationEvent {
         this._operation = operation;
         this._entity = entity;
         this._mutations = mutations;
+
+        this._changes = null;
     }
 
     /**
@@ -33,6 +35,33 @@ class EntityMutationEvent {
      */
     get mutations() {
         return this._mutations;
+    }
+
+    /**
+     * Resolves and returns the per-event delta as an object keyed by field name.
+     * The result is computed lazily on the first call and cached for subsequent calls.
+     *
+     * @public
+     * @returns {Object<string, *>}
+     */
+    getChanges() {
+        if (this._changes !== null) {
+            return this._changes;
+        }
+
+        const changes = { };
+        const serializer = this._entity.class.serializer;
+
+        for (let i = 0; i < this._mutations.length; i++) {
+            const mutation = this._mutations[i];
+            const name = serializer.getNameForFieldPath(mutation.fieldPath);
+
+            changes[name] = mutation.value;
+        }
+
+        this._changes = changes;
+
+        return changes;
     }
 }
 
