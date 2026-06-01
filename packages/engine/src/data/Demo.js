d818531d34250cec4f69314d14c86185f098a2de
Igor-Losev <igor.losev.it@gmail.com>
2026-06-01 12:44:27 +0200

    Demo: replaced entity index Map with array

--- packages/engine/src/data/Demo.js
+++ packages/engine/src/data/Demo.js
@@ -25,8 +25,12 @@ class Demo {
             byId: new Map(),
             byName: new Map()
         };
-
-        this._entities = new Map();
+        
+        this._entities = {
+            byIndex: [ ],
+            byClassName: new Map(),
+            count: 0
+        };
 
         this._serializers = new Map();
         this._server = null;
@@ -61,11 +65,16 @@ class Demo {
     deleteEntity(index) {
         Assert.isTrue(Number.isInteger(index));
 
-        const entity = this._entities.get(index) || null;
+        const entity = this._entities.byIndex[index];
 
-        if (entity !== null) {
-            this._entities.delete(index);
+        if (entity === undefined) {
+            return null;
         }
+
+        this._entities.byIndex[index] = undefined;
+        this._entities.count--;
+
+        this._removeEntityFromClassIndex(entity);
 
         return entity;
     }
@@ -113,10 +122,18 @@ class Demo {
 
     /**
      * @public
+     * @returns {IterableIterator<Class>}
+     */
+    getClassIterator() {
+        return this._classes.byId.values();
+    }
+
+    /**
+     * @public
      * @returns {Array<Entity>}
      */
     getEntities() {
-        return Array.from(this._entities.values());
+        return Array.from(this.getEntityIterator());
     }
 
     /**
@@ -125,7 +142,20 @@ class Demo {
      * @returns {Array<Entity>}
      */
     getEntitiesByClassName(className) {
-        return Array.from(this._entities.values()).filter(entity => entity.class.name === className);
+        const entities = this._entities.byClassName.get(className) || EMPTY_ENTITY_SET;
+
+        return Array.from(entities.values());
+    }
+
+    /**
+     * @public
+     * @param {string} className
+     * @returns {IterableIterator<Entity>}
+     */
+    getEntitiesByClassNameIterator(className) {
+        const entities = this._entities.byClassName.get(className) || EMPTY_ENTITY_SET;
+
+        return entities.values();
     }
 
     /**
@@ -136,7 +166,7 @@ class Demo {
     getEntity(index) {
         Assert.isTrue(Number.isInteger(index));
 
-        return this._entities.get(index) || null;
+        return this._entities.byIndex[index] || null;
     }
 
     /**
@@ -147,7 +177,25 @@ class Demo {
     getEntityByHandle(handle) {
         Assert.isTrue(Number.isInteger(handle));
 
-        return this._entities.get(handle & 0x3FFF) || null;
+        return this._entities.byIndex[handle & 0x3FFF] || null;
+    }
+
+    /**
+     * Iterates live entities in ascending index order, skipping freed slots.
+     *
+     * @public
+     * @returns {IterableIterator<Entity>}
+     */
+    * getEntityIterator() {
+        const byIndex = this._entities.byIndex;
+
+        for (let i = 0; i < byIndex.length; i++) {
+            const entity = byIndex[i];
+
+            if (entity !== undefined) {
+                yield entity;
+            }
+        }
     }
 
     /**
@@ -169,7 +217,7 @@ class Demo {
         return {
             classBaselines: this._classBaselines.size,
             classes: this._classes.byId.size,
-            entities: this._entities.size,
+            entities: this._entities.count,
             serializers: this._serializers.size
         };
     }
@@ -192,7 +240,16 @@ class Demo {
     registerEntity(entity) {
         Assert.isTrue(entity instanceof Entity);
 
-        this._entities.set(entity.index, entity);
+        const previous = this._entities.byIndex[entity.index] ?? null;
+
+        if (previous !== null) {
+            this._removeEntityFromClassIndex(previous);
+        } else {
+            this._entities.count++;
+        }
+
+        this._entities.byIndex[entity.index] = entity;
+        this._addEntityToClassIndex(entity);
     }
 
     /**
@@ -224,10 +281,46 @@ class Demo {
         this._classBaselines.clear();
         this._classes.byId.clear();
         this._classes.byName.clear();
-        this._entities.clear();
+        this._entities.byClassName.clear();
+        this._entities.byIndex.length = 0;
+        this._entities.count = 0;
         this._serializers.clear();
         this._server = null;
         this._stringTableContainer.clear();
+    }
+
+    /**
+     * @private
+     * @param {Entity} entity
+     */
+    _addEntityToClassIndex(entity) {
+        let entities = this._entities.byClassName.get(entity.class.name) || null;
+
+        if (entities === null) {
+            entities = new Set();
+
+            this._entities.byClassName.set(entity.class.name, entities);
+        }
+
+        entities.add(entity);
+    }
+
+    /**
+     * @private
+     * @param {Entity} entity
+     */
+    _removeEntityFromClassIndex(entity) {
+        const entities = this._entities.byClassName.get(entity.class.name) || null;
+
+        if (entities === null) {
+            return;
+        }
+
+        entities.delete(entity);
+
+        if (entities.size === 0) {
+            this._entities.byClassName.delete(entity.class.name);
+        }
     }
 
     /**
@@ -276,5 +369,7 @@ class Demo {
         }
     }
 }
+
+const EMPTY_ENTITY_SET = new Set();
 
 export default Demo;
