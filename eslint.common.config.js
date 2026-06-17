e89dbb8eed7ccc16241ad917b350decc6363dfa7
Igor-Losev <igor.losev.it@gmail.com>
2026-06-17 11:54:35 +0200

    Updated eslint config

--- eslint.common.config.js
+++ eslint.common.config.js
@@ -13,8 +13,6 @@ export default defineConfig([
         languageOptions: {
             globals: {
                 ...globals.browser,
-                ...globals.commonjs,
-                ...globals.jest,
                 ...globals.node
             }
         },
@@ -24,19 +22,35 @@ export default defineConfig([
         },
         rules: {
             '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
+            '@stylistic/arrow-spacing': 'error',
             '@stylistic/block-spacing': [ 'error', 'always' ],
             '@stylistic/comma-dangle': [ 'error', 'never' ],
+            '@stylistic/comma-spacing': 'error',
+            '@stylistic/eol-last': [ 'error', 'always' ],
             '@stylistic/indent': [ 'error', 4 ],
             '@stylistic/key-spacing': [ 'error', { afterColon: true, beforeColon: false, mode: 'strict' } ],
             '@stylistic/keyword-spacing': [ 'error' ],
+            '@stylistic/lines-between-class-members': [ 'error', 'always', { exceptAfterSingleLine: true } ],
+            '@stylistic/no-multiple-empty-lines': [ 'error', { max: 1 } ],
+            '@stylistic/no-trailing-spaces': 'error',
             '@stylistic/object-curly-spacing': [ 'error', 'always' ],
+            '@stylistic/quote-props': [ 'error', 'as-needed' ],
+            '@stylistic/quotes': [ 'error', 'single' ],
+            '@stylistic/semi': 'error',
             '@stylistic/space-before-blocks': [ 'error', 'always' ],
             '@stylistic/space-in-parens': [ 'error', 'never' ],
+            '@stylistic/space-infix-ops': 'error',
             '@stylistic/template-curly-spacing': [ 'error', 'never' ],
-            'no-unused-vars': [ 'error', { argsIgnorePattern: '^_', 'varsIgnorePattern': '^_' } ],
-            semi: 'error',
+            eqeqeq: [ 'error', 'always', { null: 'ignore' } ],
+            'no-promise-executor-return': 'error',
+            'no-self-compare': 'error',
+            'no-shadow': 'error',
+            'no-unreachable-loop': 'error',
+            'no-unused-expressions': 'error',
+            'no-unused-vars': [ 'error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' } ],
+            'no-var': 'error',
             'prefer-const': 'error',
-            quotes: [ 'error', 'single' ]
+            'require-atomic-updates': 'error'
         }
     }
 ]);
