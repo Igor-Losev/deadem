9bf1c98b5597dca3520062f8adb05f18696a864f
Igor-Losev <igor.losev.it@gmail.com>
2026-07-12 15:23:35 +0200

    Types: added assertion signatures to Assert

--- packages/engine/src/core/Assert.js
+++ packages/engine/src/core/Assert.js
@@ -2,8 +2,23 @@ class Assert {
     /**
      * @public
      * @static
+     * @template T
+     * @param {T} value
+     * @param {string=} message
+     * @returns {asserts value is NonNullable<T>}
+     */
+    static exists(value, message = 'Value must not be null or undefined') {
+        if (value === null || value === undefined) {
+            throw new Error(message);
+        }
+    }
+
+    /**
+     * @public
+     * @static
      * @param {boolean} condition
      * @param {string=} message
+     * @returns {asserts condition}
      */
     static isTrue(condition, message = 'Assertion failed') {
         if (!condition) {
